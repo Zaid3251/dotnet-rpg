553c386815c5990619684c44a7873bf1d328a44c
Zaid Abdullah <azaid3251@gmail.com>
2022-11-23 22:06:00 +0300

    add connection string and make migrations

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
